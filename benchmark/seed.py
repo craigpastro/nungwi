2ef57ea168c1bbf54c0e4238d971954b23c7f989
Craig Pastro <pastro.craig@gmail.com>
2022-12-11 10:48:31 -0800

    Add trivial benchmark files (#7)

---
+++ benchmark/seed.py
@@ -0,0 +1,40 @@
+import httpx
+import uuid
+
+
+def write_schema():
+    r = httpx.post(
+        "http://localhost:8000/write-schema",
+        json={
+            "configs": [
+                {"namespace": "folder", "relation": "viewer", "rewrite": "self"},
+                {
+                    "namespace": "document",
+                    "relation": "viewer",
+                    "rewrite": "union(self, tupleToUserset(parent, viewer))",
+                },
+            ]
+        },
+    )
+    assert r.status_code == httpx.codes.OK
+
+
+def write_tuples(reqs, num_tuples_per_req):
+    for i in range(reqs):
+        tuples = []
+        for j in range(num_tuples_per_req):
+            tuples.append(
+                {
+                    "namespace": "document",
+                    "id": f"{i}A{j}",
+                    "relation": "viewer",
+                    "user": "abigail",
+                }
+            )
+
+        r = httpx.post("http://localhost:8000/write-tuples", json={"tuples": tuples})
+        assert r.status_code == httpx.codes.OK
+
+
+write_schema()
+write_tuples(100, 100)
