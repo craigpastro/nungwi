f7e890f4caac5bdd445486a029ebb9f33f705e15
Craig Pastro <pastro.craig@gmail.com>
2022-12-21 16:33:31 -0800

    fix bug

--- internal/prolog/prolog.go
+++ internal/prolog/prolog.go
@@ -20,28 +20,30 @@ const zanzibar string = `
 
 checkWR(Namespace, Id, Rel, User, self) :- tuple(Namespace, Id, Rel, User).
 
-checkWR(Namespace, Id, _, User, computedUserset(Rel0)) :- tuple(Namespace, Id, Rel0, User).
+checkWR(Namespace, Id, _, User, computedUserset(Rel0)) :-
+    config(Namespace, Rel0, Rewrite),
+    checkWR(Namespace, Id, Rel0, User, Rewrite).
 
-checkWR(Namespace, Id, _, User, tupleToUserset(S, T)) :-
-    tuple(Namespace, Id, S, object(Namespace0, Id0)),
-    config(Namespace0, T, Rewrite),
-    checkWR(Namespace0, Id0, T, User, Rewrite).
+checkWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+    tuple(Namespace, Id, Tupleset, object(Namespace0, Id0)),
+    config(Namespace0, ComputedUserset, Rewrite),
+    checkWR(Namespace0, Id0, ComputedUserset, User, Rewrite).
 
-checkWR(Namespace, Id, _, User, tupleToUserset(S, T)) :-
-    tuple(Namespace, Id, S, userset(Namespace0, Id0, T)),
-    config(Namespace0, T, Rewrite),
-    checkWR(Namespace0, Id0, T, User, Rewrite).
+checkWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+    tuple(Namespace, Id, Tupleset, userset(Namespace0, Id0, ComputedUserset)),
+    config(Namespace0, ComputedUserset, Rewrite),
+    checkWR(Namespace0, Id0, ComputedUserset, User, Rewrite).
 
-checkWR(Namespace, Id, Rel, User, union(S, _)) :- checkWR(Namespace, Id, Rel, User, S).
-checkWR(Namespace, Id, Rel, User, union(_, T)) :- checkWR(Namespace, Id, Rel, User, T).
+checkWR(Namespace, Id, Rel, User, union(L, _)) :- checkWR(Namespace, Id, Rel, User, L).
+checkWR(Namespace, Id, Rel, User, union(_, R)) :- checkWR(Namespace, Id, Rel, User, R).
 
-checkWR(Namespace, Id, Rel, User, intersection(S, T)) :-
-    checkWR(Namespace, Id, Rel, User, S),
-    checkWR(Namespace, Id, Rel, User, T).
+checkWR(Namespace, Id, Rel, User, intersection(L, R)) :-
+    checkWR(Namespace, Id, Rel, User, L),
+    checkWR(Namespace, Id, Rel, User, R).
 
-checkWR(Namespace, Id, Rel, User, exclusion(S, T)) :-
-    checkWR(Namespace, Id, Rel, User, S),
-    \+ checkWR(Namespace, Id, Rel, User, T).
+checkWR(Namespace, Id, Rel, User, exclusion(M, S)) :-
+    checkWR(Namespace, Id, Rel, User, M),
+    \+ checkWR(Namespace, Id, Rel, User, S).
 
 check(Namespace, Id, Rel, User) :-
     config(Namespace, Rel, Rewrite),
